11db96c3e9d9808db50256b0f7ce4daab8424413
keerkavi <keerthanalakshmi1995@gmail.com>
2023-08-11 16:45:46 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,9 +38,9 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 
 
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
-streamlit.text("My fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.header("My fruit load list contains:")
+streamlit.dataframe(my_data_row)
 
