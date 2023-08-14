219813e414b8fb2629f4998b43310216334d9429
keerkavi <keerthanalakshmi1995@gmail.com>
2023-08-14 12:39:58 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ def insert_row_snowflake(new_fruit):
          #my_cur.execute("Select * from fruit_load_list")
          #my_cur.execute("insert into fruit_load_list values('from streamlit')")
           my_cur.execute("insert into fruit_load_list values('" +new_fruit+"')")
-         return "Thanks for adding" + new_fruit
+          return "Thanks for adding" + new_fruit
          #return my_cur.fetchall()
 add_my_fruit=streamlit.text_input('what fruit would you like to add?')
 if streamlit.button('Get Fruit List'):
